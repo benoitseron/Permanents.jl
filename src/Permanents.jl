4c0155a65553760c728c06eb9dc0eea6cffe0cd5
benoitseron <benoitseron@gmail.com>
2022-01-17 19:40:57 +0100

    naive permanents

--- src/Permanents.jl
+++ src/Permanents.jl
@@ -1,5 +1,33 @@
+using Combinatorics
+using LinearAlgebra
+
+const ATOL = 1e4 * eps()
+
 module Permanents
 
-# Write your package code here.
+function naive(U::AbstractMatrix)
+
+    """ computes the permanent using the definition as a naive implementation"""
+
+    n = size(U)[1]
+
+    sum_diag(perm, U) = prod(U[i,perm[i]] for i = 1:n)
+
+    return sum(sum_diag(sigma, U) for sigma in permutations(collect(1:n)))
+
+end
+
+function naive_tensor(W::Array)
+
+    if length(size(W)) == 3 && all(size(W) .== size(W)[1])
+        n = size(W)[1]
+
+        sum_tensor_diag(sigma,rho, W) = prod(W[i,sigma[i],rho[i]] for i = 1:n)
+        return sum(sum_tensor_diag(sigma,rho, W) for sigma in permutations(collect(1:n)) for rho in permutations(collect(1:n)))
+    else
+        error("tensor permanent implemented only for square 3-indices tensors")
+    end
+
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
-using Permanents
+using Main.Permanents
 using Test
 
+function fourier_matrix(n; normalized = true)
+
+    U = Array{ComplexF64}(undef, n, n)
+
+    for i in 1:n
+        for j in 1:n
+            U[i,j] = exp((2im * pi / n)* (i-1) * (j-1))
+        end
+    end
+
+    if normalized
+        return 1/sqrt(n) * U
+    else
+        return U
+    end
+end
+
 @testset "Permanents.jl" begin
-    # Write your tests here.
+    theoretical_permanents_fourier_matrix = [1,0,-3,0,-5,0,-105,0,81,0,6765,0,175747,0,30375,0,25219857,0,142901109,0,4548104883,0]
+    #from "on the permanent of schur matrix", graham
+
+    for n = 1:6
+
+        @test naive(ones(Int,n,n)) == factorial(n)
+        @test naive_tensor(ones(n,n,n)) ≈ factorial(n)^2
+
+        U = fourier_matrix(n, normalized = false)
+        @test naive(U) ≈ theoretical_permanents_fourier_matrix[n] atol=ATOL
+    end
+
 end
