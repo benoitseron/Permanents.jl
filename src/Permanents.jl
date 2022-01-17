6624a48ef609da58af95c250fcc6e1937e982b79
benoitseron <benoitseron@gmail.com>
2022-01-17 20:52:39 +0100

    ryser

--- src/Permanents.jl
+++ src/Permanents.jl
@@ -30,4 +30,64 @@ function naive_tensor(W::Array)
 
 end
 
+function ryser(A::AbstractMatrix)
+
+	"""computes the permanent of A using Ryser's algorithm with Gray ordering"""
+
+	# see Combinatorial Algorithms for Computers and Calculators,
+	# Second Edition (Computer Science and Applied Mathematics)
+	# by Albert Nijenhuis, Herbert S. Wilf (z-lib.org)
+	# chapter 23 for the permanent algorithm
+	# chapter 1 for the gray code
+
+    n = length(A[1,:])
+    if length(A[1,:]) != length(A[:,1])
+        error("matrix not square")
+    end
+
+    a = zeros(Int, n)
+    f = collect(0:n)
+    f[n+1] = n
+
+    j = 0
+
+    total = zero(eltype(A))
+    sums = zeros(eltype(A), n)
+	one_this_type = one(eltype(A))
+
+    @inbounds for parity = 0 : 2^n-2
+
+        j = f[1]
+        f[1] = 0
+
+        f[j+1] = f[j+2]
+        f[j+2] = j + 1
+        a[j+1] = 1 - a[j+1] # there you find the bit to flip, j+1
+
+        this_product = one_this_type
+
+        if a[j+1] == 1
+            for i = 1 : n
+                sums[i] +=  A[j+1, i]
+				this_product *= sums[i]
+            end
+        else
+            for i = 1 : n
+                sums[i] -=  A[j+1, i]
+				this_product *= sums[i]
+            end
+        end
+
+		if parity % 2 == 0
+        	total += this_product
+		else
+			total -= this_product
+		end
+
+    end
+
+    (-1)^(n-1) * total
+
+end
+
 end
