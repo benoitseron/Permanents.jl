9d50a1a88a5ce6ec728d8534c5ceee1da1535e27
Antoine Restivo <71372614+AntoineRestivo@users.noreply.github.com>
2022-02-03 22:11:00 +0100

    added multi_dim_ryser

--- src/Permanents.jl
+++ src/Permanents.jl
@@ -1,8 +1,8 @@
 module Permanents
 
-export naive, naive_tensor, ryser, fast_glynn_perm
+export naive, naive_tensor, ryser
 
-using Combinatorics:permutations
+using Combinatorics
 using LinearAlgebra
 
 function naive(U::AbstractMatrix)
@@ -28,6 +28,41 @@ function naive_tensor(W::Array)
         error("tensor permanent implemented only for square 3-indices tensors")
     end
 
+end
+
+function multi_dim_ryser(U, gram_matrix)
+
+    # https://arxiv.org/pdf/1410.7687.pdf
+
+    n = size(U)[1]
+    nstring = collect(1:n)
+    sub_nstring = collect(powerset(nstring))
+    sub_nstring = sub_nstring[2:length(sub_nstring)]
+    res = 0
+
+    function delta_set(S1, S2)
+        if S1 == S2
+            return 1
+        else
+            return 0
+        end
+    end
+
+    for r = 1:length(sub_nstring)
+        for s = r:length(sub_nstring)
+            R = sub_nstring[r]
+            S = sub_nstring[s]
+
+            t = prod(
+                sum(
+                    U[ss, j] * conj(U[rr, j]) * gram_matrix[rr, ss] for rr in R for ss in S
+                ) for j = 1:n
+            )
+            res +=
+                (2 - delta_set(S, R)) * (-1)^(length(S) + length(R)) * real(t)
+        end
+    end
+    return res
 end
 
 function ryser(A::AbstractMatrix)
@@ -85,8 +120,6 @@ function ryser(A::AbstractMatrix)
 		else
 			throw(ArgumentError("perm: argument must be a square matrix"))
 		end
-
-end
 
 end
 
