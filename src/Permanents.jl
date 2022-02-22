3c10c70ec3c4b84c328770d7a12d814be90b1683
benoitseron <benoitseron@gmail.com>
2022-02-22 22:51:27 +0100

    ryser_tensor

--- src/Permanents.jl
+++ src/Permanents.jl
@@ -1,9 +1,10 @@
 module Permanents
 
-export naive, naive_tensor, ryser, multi_dim_ryser, fast_glynn_perm, glynn, glynn_precision
+export naive, naive_tensor, ryser, multi_dim_ryser, fast_glynn_perm, glynn, glynn_precision, ryser_tensor
 
 using Combinatorics
 using LinearAlgebra
+using ArgCheck
 
 function naive(U::AbstractMatrix)
 
@@ -16,10 +17,16 @@ function naive(U::AbstractMatrix)
     return sum(sum_diag(sigma, U) for sigma in permutations(collect(1:n)))
 
 end
+function is_a_square_three_tensor(W::Array)
+
+	length(size(W)) == 3 && all(size(W) .== size(W)[1])
+
+end
+
 
 function naive_tensor(W::Array)
 
-    if length(size(W)) == 3 && all(size(W) .== size(W)[1])
+    if is_a_square_three_tensor(W)
         n = size(W)[1]
 
         sum_tensor_diag(sigma,rho, W) = prod(W[i,sigma[i],rho[i]] for i = 1:n)
@@ -30,7 +37,51 @@ function naive_tensor(W::Array)
 
 end
 
+function ryser_tensor(W::Array)
+
+	"""implements the tensor permanent according to Ryser's factorization
+	without using Gray factoring
+
+	corresponds to Eq. 21 of https://arxiv.org/abs/1410.7687"""
+
+	@argcheck is_a_square_three_tensor(W) "tensor permanent implemented only for square 3-indices tensors"
+
+	n = size(U)[1]
+    nstring = collect(1:n)
+    sub_nstring = collect(powerset(nstring))
+    sub_nstring = sub_nstring[2:length(sub_nstring)]
+    res = 0
+
+    function delta_set(S1, S2)
+        if S1 == S2
+            return 1
+        else
+            return 0
+        end
+    end
+
+    for r = 1:length(sub_nstring)
+        for s = r:length(sub_nstring)
+            R = sub_nstring[r]
+            S = sub_nstring[s]
+
+            t = prod(
+                sum(
+					W[rr,ss,j] for rr in R for ss in S
+                ) for j = 1:n
+            )
+            res +=
+                (2 - delta_set(S, R)) * (-1)^(length(S) + length(R)) * real(t)
+        end
+    end
+    return res
+
+end
+
+
 function multi_dim_ryser(U, gram_matrix)
+
+	@warn "obsolete, please convert to ryser_tensor(W::Array)"
 
     # https://arxiv.org/pdf/1410.7687.pdf
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,35 @@ function fourier_matrix(n; normalized = true)
     end
 end
 
+function rand_haar(n::Int)
+
+    """generates a Haar distributed unitary matrix of size n*n"""
+    # follows https://case.edu/artsci/math/esmeckes/Meckes_SAMSI_Lecture2.pdf
+
+    qr(randn(ComplexF64, n,n)).Q
+end
+
+
+function rand_gram_matrix(n::Int)
+
+	"""random gram matrix with full rank """
+
+	function normalized_random_vector(n)
+		v = rand(ComplexF64, n)
+		1/norm(v) .* v
+	end
+
+	generating_vectors = 0.
+	while det(generating_vectors) ≈ 0. #check it's a basis
+	    generating_vectors = hcat([normalized_random_vector(n) for i = 1:n]...)
+
+	end
+
+	generating_vectors' * generating_vectors
+
+end
+
+
 @testset "Permanents.jl" begin
     theoretical_permanents_fourier_matrix = [1,0,-3,0,-5,0,-105,0,81,0,6765,0,175747,0,30375,0,25219857,0,142901109,0,4548104883,0]
     #from "on the permanent of schur matrix", graham
@@ -36,7 +65,25 @@ end
         @test multi_dim_ryser(U, ones(n,n)) ≈ abs(theoretical_permanents_fourier_matrix[n]).^2 atol=ATOL
     end
 
+	### consistency of ryser_tensor(W) ≈  multi_dim_ryser(U, S) ###
+
     U = fourier_matrix(7,normalized = false)
     @test glynn_precision(U; rtol = 1e-3, maxiter = 10^5) ≈ theoretical_permanents_fourier_matrix[7] rtol = 1e-2
+
+	n = 7
+	U = rand_haar(n)
+	S = rand_gram_matrix(n)
+
+	W = Array{eltype(S)}(undef, (n,n,n))
+
+	for ss in 1:n
+	    for rr in 1:n
+	        for j in 1:n
+	            W[ss,rr,j] = U[ss, j] * conj(U[rr, j]) * S[rr, ss]
+	        end
+	    end
+	end
+
+	@test ryser_tensor(W) ≈  multi_dim_ryser(U, S)
 
 end
